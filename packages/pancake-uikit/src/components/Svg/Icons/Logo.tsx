42ceea184efdaa9ca0fae650086c29fb7afea46c
丁文軍 <eric_dwj@icloud.com>
2021-09-27 00:44:58 +0900

    Merge branch 'mole' of https://github.com/MoleTech-io/pancake-toolkit into mole

--- packages/pancake-uikit/src/components/Svg/Icons/Logo.tsx
+++ packages/pancake-uikit/src/components/Svg/Icons/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Svg from "../Svg";
 import { SvgProps } from "../types";
 
-const Icon: React.FC<SvgProps> = (props) => {
+const Icon: React.FC<SvgProps> = () => {
   return (
     <Svg viewBox="0 0 32 32" {...props} />
   );
